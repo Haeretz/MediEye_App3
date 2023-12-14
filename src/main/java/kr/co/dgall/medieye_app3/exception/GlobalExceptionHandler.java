7770b216417a36d6951d4c008d4dbcf1d87f4928
ycu <yechan3678@gmail.com>
2023-12-14 18:16:15 +0900

    resources 이미지,css 파일 추가 및 적용

---
+++ src/main/java/kr/co/dgall/medieye_app3/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package kr.co.dgall.medieye_app3.exception;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import kr.co.dgall.medieye_app3.dto.ApiResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@Autowired
+    private MessageSource messageSource;
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ApiResponse<?> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+		return ApiResponse.createError(messageSource.getMessage("error.already.exist.email", null, Locale.getDefault()));
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+    public ApiResponse<?> handleNullPointerException(NullPointerException ex) {
+        return ApiResponse.createError(messageSource.getMessage("error.already.exist.email", null, Locale.getDefault()));
+    }
+
+}
